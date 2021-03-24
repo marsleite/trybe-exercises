abe3c776f8994748da8e1bf016f802fea58a66d7
Marcelo Leite <marsleite@gmail.com>
2021-03-24 19:29:13 -0300

    questão 2 resolvida.

--- exercises/introducao-javascript_1/exercicios001.js
+++ exercises/introducao-javascript_1/exercicios001.js
@@ -9,3 +9,14 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// 2 - Programa
+
+let num1 = 10;
+let num2 = 6;
+
+if (num1 > num2) {
+    console.log(`O valor ${num1} é maior que ${num2}`)
+} else {
+    console.log(`o valor ${num2} é maior que ${num1}`)
+}
