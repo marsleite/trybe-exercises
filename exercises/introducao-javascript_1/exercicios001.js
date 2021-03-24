dbddea5cc2b49b84635717718c584991512cd61f
Marcelo Leite <marsleite@gmail.com>
2021-03-24 19:40:10 -0300

    terceira parte do exercicio.

--- exercises/introducao-javascript_1/exercicios001.js
+++ exercises/introducao-javascript_1/exercicios001.js
@@ -16,7 +16,21 @@ let num1 = 10;
 let num2 = 6;
 
 if (num1 > num2) {
-    console.log(`O valor ${num1} é maior que ${num2}`)
+    console.log(`O valor ${num1} é maior que ${num2}`);
 } else {
-    console.log(`o valor ${num2} é maior que ${num1}`)
+    console.log(`o valor ${num2} é maior que ${num1}`);
+}
+
+//3- Programa
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 2;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(`O valor ${num1} é maior dentre os 3 números`);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(`O valor ${num2} é maior dentre os 3 números`);
+} else {
+  console.log(`O valor ${num3} é maior dentre os 3 números`);
 }
