46e5fd649e6da23907fa6b45e6df603f7c0f4c03
Marcelo Leite <marsleite@gmail.com>
2021-03-25 19:26:11 -0300

    questão 4 resolvida

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
@@ -3,24 +3,33 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 1 questão
 
 for (let index = 0; index < numbers.length; index += 1) {
-    let result = numbers[index]
+    let result = numbers[index];
     console.log(result);
 }
-console.log("")
+console.log("");
 
 // 2 Questão
 
 let soma = 0;
 for (i = 0; i < numbers.length; i += 1) {
- soma = soma + numbers[i]  
+ soma = soma + numbers[i];  
 }
-console.log(soma)
-console.log("")
+console.log(soma);
+console.log("");
 // 3 Questão
 
 let soma3 = 0;
 for (let i = 0; i < numbers.length; i += 1) {
-    soma3 = soma3 + numbers[i]
+    soma3 = soma3 + numbers[i];
 }
-let media = soma3 / numbers.length
-console.log(media)
+let media = soma3 / numbers.length;
+console.log(media);
+console.log("");
+
+// 4 Questao
+
+if(media > 20){
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor que 20');
+}
