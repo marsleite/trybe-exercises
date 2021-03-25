277ee7b5b52c52adcc75e9672cbb05c38f4d47d4
Marcelo Leite <marsleite@gmail.com>
2021-03-25 19:11:10 -0300

    questão 2 resolvida

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
@@ -1,8 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//1 questão
+// 1 questão
 
 for (let index = 0; index < numbers.length; index += 1) {
     let result = numbers[index]
-    console.log(result)
+    console.log(result);
 }
+console.log("")
+
+// 2 Questão
+
+let soma = 0;
+for (i = 0; i < numbers.length; i += 1) {
+ soma = soma + numbers[i]  
+}
+console.log(soma)
