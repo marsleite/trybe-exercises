4cb3af24d1238097387f8fd4480b7e1a579f969c
Marcelo Leite <marsleite@gmail.com>
2021-03-25 18:32:11 -0300

    Questão 1 resolvida.

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercises.js
@@ -1,1 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1 questão
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let result = numbers[index]
+    console.log(result)
+}
