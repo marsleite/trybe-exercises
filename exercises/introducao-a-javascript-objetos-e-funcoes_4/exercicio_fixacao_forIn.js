bf9eb4c56f32ccdd21f08dbe7124285773b09dd5
Marcelo Leite <marsleite@gmail.com>
2021-03-29 15:58:44 -0300

    segunda parte do fixação for/in.

--- exercises/introducao-a-javascript-objetos-e-funcoes_4/exercicio_fixacao_forIn.js
+++ exercises/introducao-a-javascript-objetos-e-funcoes_4/exercicio_fixacao_forIn.js
@@ -1,9 +1,20 @@
 let names = {
-    person1: 'João', 
-    person2: 'Maria', 
-    person3: 'Jorge',
+  person1: 'João', 
+  person2: 'Maria', 
+  person3: 'Jorge',
 }
 
 for(let index in names) {
-    console.log(`Olá ${names[index]}`);
+  console.log(`Olá ${names[index]}`);
+}
+console.log("");
+
+let cars = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+}
+
+for (let key in cars) {
+  console.log(`${key}: ${cars[key]}`);
 }
