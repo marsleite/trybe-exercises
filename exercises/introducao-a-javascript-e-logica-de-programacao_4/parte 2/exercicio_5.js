f06955591a193b383b72feb561a7323542930af2
Marcelo Leite <marsleite@gmail.com>
2021-04-05 18:23:44 -0300

    resolução exercicio_02

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/parte 2/exercicio_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/parte 2/exercicio_5.js
@@ -1,18 +1,47 @@
-function maiorValor (maior){
-  let valores = [2, 3, 2, 5, 8, 2, 3];
-  let contador = {};
-  for (let i = 0; i < valores.length; i += 1){
-    if (contador[valores[i]]) {
-      contador[valores[i]] += 1;
-    } else {
-      contador[valores[i]] = 1;
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
     }
-  }
-  for (let j in contador) {
-    if (contador[j] >= 2) {
-      maior = contador[j];
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
     }
+    contNumero = 0;
   }
-};
-console.log(maiorValor(maior));
+  return numeros[indexNumeroRepetido];
+}
+let phoneNumber = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 0, 1, ];
+console.log(maisRepetido(phoneNumber));
 
+var array = [1, 1, 1 , 1, 2];
+var arrayVerificado = [];
+
+for(var i = 0; i < array.length; i++){
+	var contador = 0;  
+  var repetiu = false;
+  
+  for(var z = 0; z < arrayVerificado.length; z++){
+  	if(arrayVerificado[z] == array[i]){
+     //Verifico se no array auxiliar, o número já foi verificado, caso sim, seto a variavel repetiu para true.
+    	repetiu = true
+    }
+  }  
+  if(repetiu == false){ //caso não tenha repetido, eu deixo o padrão false.
+  arrayVerificado.push(array[i])//add no meu array auxiliar que este número vai ser verificado
+    for(var j = 0; j < array.length; j++){
+      if(array[i] == array[j]){ //se ele achar, faço meu contador acrescentar +1
+        contador++
+      }
+      
+    }
+      console.log(array[i]);
+  }
+
+}
