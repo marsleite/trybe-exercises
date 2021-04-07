0a6f788d77fcc3e1d83ffb9fabadd66d6e609fb5
Marcelo Leite <marsleite@gmail.com>
2021-04-07 20:45:07 -0300

    travado no conteudo do dia exercicio

--- exercises/javascript-dom-eventos-e-web-storage_3/conteudo_do_dia/main.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/conteudo_do_dia/main.js
@@ -5,13 +5,20 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
 let elementFather = document.body;
+let mainContainer = document.querySelector('.container');
+
 /*
  1 -Crie uma função que adicione a classe 'tech' ao elemento selecionado; */
  function addClassTech(event) {
   event.target.className = 'tech';
  }
-/* 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+/* 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
+function removeTech() {
+  let removeClass = mainContainer.querySelector('.tech');
+  removeClass.classList.remove('tech');
+}
+
+/* Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
@@ -28,7 +35,9 @@ function resetText(event) {
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
-divUm.addEventListener('dblclick', resetText);
+addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
+addEventListener('click', addClassTech);
+addEventListener('click', removeTech);
