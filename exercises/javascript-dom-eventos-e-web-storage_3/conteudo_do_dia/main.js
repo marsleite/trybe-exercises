d6265217e54ff499d07f5fd9d8bfebd19703a911
Marcelo Leite <marsleite@gmail.com>
2021-04-07 19:41:08 -0300

    iniciando o exercicio conteudo do dia, questao 1.

--- exercises/javascript-dom-eventos-e-web-storage_3/conteudo_do_dia/main.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/conteudo_do_dia/main.js
@@ -4,10 +4,13 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
+let elementFather = document.body;
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+ 1 -Crie uma função que adicione a classe 'tech' ao elemento selecionado; */
+ function addClassTech(event) {
+  event.target.className = 'tech';
+ }
+/* 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
@@ -25,7 +28,6 @@ function resetText(event) {
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
-
 divUm.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
