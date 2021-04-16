40ec44c963a3030a92c0bb6064db85c24b9f4783
Marcelo Leite <marsleite@gmail.com>
2021-04-16 19:11:33 -0300

    criação dos estados por script

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,40 @@
+const selectStates = document.getElementById('input-states');
+const brazilStates = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'Ap': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espirito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins' 
+}
+
+function states(uf) {
+  for (let key in uf) {
+    let item = document.createElement('option');
+    item.innerText = uf[key];
+    selectStates.appendChild(item);
+  }
+}
+
+states(brazilStates);
