f2771bd417142be4f3470ac095c68abd587954ee
Marcelo <marsleite@gmail.com>
2021-04-28 23:33:24 -0300

    mais exercicios de fixação.

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/fixacao02.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/fixacao02.js
@@ -7,3 +7,5 @@ function division(x, y) {
 const expected = division(9, 3);
 
 assert.strictEqual(expected, 3, "nove dividido por três é igual a três")
+
+assert.strictEqual(expected, '3', "nove dividido por três é igual a três")
