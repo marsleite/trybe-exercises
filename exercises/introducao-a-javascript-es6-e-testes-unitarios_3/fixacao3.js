4f6bc258f90fb352c1530595608b5513feb8dfd3
Marcelo <marsleite@gmail.com>
2021-04-28 23:36:34 -0300

    mais exercicios de fixação realizado

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/fixacao3.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+function add(a, b) {
+    return a + b;
+}
+
+const expected = add(1, 2);
+
+assert.ok(expected === 3,'Um mais dois é igual a três');
+
+assert.strictEqual(expected, 3, 'Um mais dois é igual a três');
+ assert.notStrictEqual(expected, 4, 'Um mais doiss é igual a três');
